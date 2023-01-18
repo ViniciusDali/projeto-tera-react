57726cd9d0361cbe54a7ebc2e996b5b7d4cf1260
cassiolacerda <697508+cassiolacerda@users.noreply.github.com>
2023-01-18 20:51:12 -0300

    Migra HTML do componente da página inicial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Home from "./components/pages/Home";
+
+import "./styles/normalize.css";
+import "./styles/fontawesome.min.css";
+import "./styles/main.css";
+
 function App() {
-  return <div>Projeto</div>;
+  return <Home />;
 }
 
 export default App;
