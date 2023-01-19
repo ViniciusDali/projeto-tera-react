7a413f2d160db3f4a5b57b1eb90b87564afafb42
cassiolacerda <697508+cassiolacerda@users.noreply.github.com>
2023-01-18 22:06:47 -0300

    Adiciona handlers e states para página inicial

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import logo from "../../images/logo.svg";
 
 export default function Home() {
+  const [users, setUsers] = React.useState([]);
+
+  React.useEffect(() => console.log("depois do return"), []);
+
+  const handleUserChange = () => console.log("onChange");
+  const handleConfirmClick = () => console.log("onClick");
+
+  console.log("antes do return");
+
   return (
     <div className="home center">
       <div className="home__logo">
         <img src={logo} className="responsive" alt="" />
       </div>
-      <select className="home__select-users">
-        <option>User 1</option>
-        <option>User 2</option>
-        <option>User 3</option>
-      </select>
-      <button className="button-primary">Entrar</button>
+      <select
+        onChange={handleUserChange}
+        className="home__select-users"
+      ></select>
+      <button onClick={handleConfirmClick} className="button-primary">
+        Entrar
+      </button>
     </div>
   );
 }
