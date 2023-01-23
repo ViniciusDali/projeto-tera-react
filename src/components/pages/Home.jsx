a8965b745a8bde857ac1df6018606199ee6c8625
cassiolacerda <697508+cassiolacerda@users.noreply.github.com>
2023-01-23 20:39:19 -0300

    Adciona página inicial completa

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,28 +1,45 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import logo from "../../images/logo.svg";
 
 export default function Home() {
+  const navigate = useNavigate();
+
   const [users, setUsers] = React.useState([]);
+  const [currentUser, setCurrentUser] = React.useState("");
 
-  React.useEffect(() => console.log("depois do return"), []);
+  React.useEffect(() => {
+    fetch("https://63cf09718a780ae6e6710dbe.mockapi.io/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
 
-  const handleUserChange = () => console.log("onChange");
-  const handleConfirmClick = () => console.log("onClick");
+  const handleUserChange = (e) => setCurrentUser(e.target.value);
 
-  console.log("antes do return");
+  const handleConfirmClick = () => {
+    navigate(`/users/${currentUser}`);
+  };
 
   return (
     <div className="home center">
       <div className="home__logo">
         <img src={logo} className="responsive" alt="" />
       </div>
-      <select
-        onChange={handleUserChange}
-        className="home__select-users"
-      ></select>
-      <button onClick={handleConfirmClick} className="button-primary">
-        Entrar
-      </button>
+      <select onChange={handleUserChange} className="home__select-users">
+        <option value="">Selecionar usuário</option>
+        {users
+          .sort((a, b) => a.fn.localeCompare(b.fn))
+          .map((user) => (
+            <option key={user.id} value={user.id}>
+              {user.fn} {user.ln}
+            </option>
+          ))}
+      </select>
+      {!!currentUser && (
+        <button onClick={handleConfirmClick} className="button-primary">
+          Entrar
+        </button>
+      )}
     </div>
   );
 }
