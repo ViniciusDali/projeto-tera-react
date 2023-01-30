7890f89d3744102e95feb171fd9a0e4eb3b35b5d
ViniciusDali <112828047+ViniciusDali@users.noreply.github.com>
2023-01-30 17:31:58 -0300

    modificado

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 
-import logo from "../../images/logo.svg";
+//import logo from "../../images/logo.svg";
 import user1 from "../../images/placeholders/user-1.jpg";
 import user2 from "../../images/placeholders/user-2.jpg";
 import user3 from "../../images/placeholders/user-3.jpg";
